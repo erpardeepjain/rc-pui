f3dc25b8b87c6a029bab094338daa4dfe8dc4c52
erpardeepjain <erpardeepjain@gmail.com>
2023-01-30 18:28:21 +0530

    Search Component added into build

---
+++ dist/SearchInput/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-underscore-dangle */
+const SearchInput = ({
+  onChange,
+  placeholder,
+  waitTime,
+  minSearchChar
+}) => {
+  let tempInterval = '';
+  const _handleChange = val => {
+    const searchedKeywrod = val.target.value.trim();
+    clearTimeout(tempInterval);
+    tempInterval = setTimeout(() => {
+      const qp = searchedKeywrod.length > (minSearchChar || 2) ? searchedKeywrod : "";
+      onChange(qp);
+    }, waitTime || 500);
+  };
+  return /*#__PURE__*/React.createElement("div", {
+    className: "search-box"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "position-relative"
+  }, /*#__PURE__*/React.createElement("input", {
+    className: "form-control",
+    type: "search",
+    onChange: _handleChange,
+    placeholder: placeholder || "Search"
+  }), /*#__PURE__*/React.createElement("i", {
+    className: "mdi mdi-magnify search-icon"
+  })));
+};
+export default SearchInput;
