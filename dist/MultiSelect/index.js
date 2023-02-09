826e34f68ed1b6b6ba0b07bc4e2ee780310bfbd1
erpardeepjain <erpardeepjain@gmail.com>
2023-02-09 18:37:52 +0530

    [MultiSelect]: Outside click handle, All Option added

--- dist/MultiSelect/index.js
+++ dist/MultiSelect/index.js
@@ -1,30 +1,29 @@
 /* eslint-disable no-underscore-dangle */
 import React, { useState, useEffect, useRef } from 'react';
 import './index.css';
-function useOutsideAlerter(ref) {
+const MultiSelect = ({
+  name,
+  title,
+  options,
+  onSelect,
+  selectAll = false
+}) => {
+  const wrapperRef = useRef(null);
+  const [allSelected, setAllSelected] = useState(false);
+  const [selectionArr, setSelectionArr] = useState([]);
+  const [optionsList, setOptionsList] = useState([]);
+  const [showList, setShowList] = useState(false);
   useEffect(() => {
     const handleClickOutside = event => {
-      if (ref.current && !ref.current.contains(event.target)) {
-        console.log("You clicked outside of me!");
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target) && showList) {
+        setShowList(false);
       }
     };
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [ref]);
-}
-const MultiSelect = ({
-  name,
-  label,
-  options,
-  onSelect
-}) => {
-  const wrapperRef = useRef(null);
-  useOutsideAlerter(wrapperRef);
-  const [showList, setShowList] = useState(false);
-  const [selectionArr, setSelectionArr] = useState([]);
-  const [optionsList, setOptionsList] = useState([]);
+  }, [wrapperRef, showList]);
   useEffect(() => {
     const alterListArr = [];
     options?.forEach(ele => {
@@ -37,17 +36,30 @@ const MultiSelect = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const _onSelect = val => {
-    const _selectionArr = selectionArr;
-    const itemIndex = _selectionArr.indexOf(val?.label);
-    if (itemIndex !== -1) {
-      val.selected = false;
-      _selectionArr.splice(itemIndex, 1);
+    if (val === "all") {
+      setSelectionArr(!allSelected ? options : []);
+      onSelect(!allSelected ? options : [], name);
+      setAllSelected(!allSelected);
+      optionsList.forEach(ele => ele.selected = false);
+      setShowList(false);
     } else {
-      val.selected = true;
-      _selectionArr.push(val?.label);
+      const _selectionArr = selectionArr;
+      const itemIndex = _selectionArr.indexOf(val?.label);
+      if (itemIndex !== -1) {
+        val.selected = false;
+        _selectionArr.splice(itemIndex, 1);
+      } else {
+        val.selected = true;
+        _selectionArr.push(val?.label);
+      }
+      setSelectionArr(_selectionArr);
+      onSelect(selectionArr, name);
+      if (_selectionArr?.length === options?.length) {
+        setAllSelected(true);
+      } else {
+        setAllSelected(false);
+      }
     }
-    setSelectionArr(_selectionArr);
-    onSelect(selectionArr, name);
   };
   return /*#__PURE__*/React.createElement("div", {
     className: "multiSelectBox",
@@ -55,16 +67,27 @@ const MultiSelect = ({
   }, /*#__PURE__*/React.createElement("span", {
     onClick: () => setShowList(!showList),
     className: "selectLabel"
-  }, label || 'Select Multiple', /*#__PURE__*/React.createElement("span", {
+  }, title || 'Select Multiple', /*#__PURE__*/React.createElement("span", {
     className: showList ? "arrow_up" : "arrow_down"
   })), /*#__PURE__*/React.createElement("div", {
     className: "listItemsBox"
-  }, showList && optionsList?.length > 0 && optionsList.map(ele => /*#__PURE__*/React.createElement("span", {
+  }, showList && selectAll && /*#__PURE__*/React.createElement("span", {
+    onClick: () => _onSelect("all"),
+    value: "all",
+    className: "selectItem"
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "checkbox",
+    readOnly: true,
+    checked: allSelected
+  }), "All"), showList && optionsList?.length > 0 && optionsList.map(ele => /*#__PURE__*/React.createElement("span", {
     onClick: () => _onSelect(ele),
+    key: `multiSelect-${ele}`,
     value: ele?.label,
     className: "selectItem"
-  }, /*#__PURE__*/React.createElement("span", {
-    className: ele?.selected ? "selectedFlag" : "unselectedFlag"
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "checkbox",
+    readOnly: true,
+    checked: ele?.selected || allSelected
   }), ele?.label[0].toUpperCase() + ele?.label?.slice(1, ele?.label?.length)))));
 };
 export default MultiSelect;
--- dist/Pagination/index.js
+++ dist/Pagination/index.js
@@ -52,6 +52,7 @@ const Pagination = ({
     if (i + 1 >= start && i + 1 <= end) {
       dom = /*#__PURE__*/React.createElement("span", {
         tabIndex: i,
+        key: `pagination${i}`,
         role: "button",
         className: i + 1 === _currentPage ? 'active page' : 'page',
         onClick: () => _onPageChange(i + 1),
